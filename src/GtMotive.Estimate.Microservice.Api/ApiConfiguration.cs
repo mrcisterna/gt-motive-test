5858865c34a7d08971ff854cb59537d9aed903cc
martin <martincisterna@hotmail.com>
2025-12-27 00:38:48 +0100

    Fix PipeWriter.UnflushedBytes issue - use CompatibleJsonOutputFormatter

--- src/GtMotive.Estimate.Microservice.Api/ApiConfiguration.cs
+++ src/GtMotive.Estimate.Microservice.Api/ApiConfiguration.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection;
+using System.Text.Json;
 using GtMotive.Estimate.Microservice.Api.Authorization;
 using GtMotive.Estimate.Microservice.Api.DependencyInjection;
 using GtMotive.Estimate.Microservice.Api.Filters;
+using GtMotive.Estimate.Microservice.Api.Formatters;
 using GtMotive.Estimate.Microservice.ApplicationCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +23,16 @@ namespace GtMotive.Estimate.Microservice.Api
             ArgumentNullException.ThrowIfNull(options);
 
             options.Filters.Add<BusinessExceptionFilter>();
+
+            // Replace SystemTextJsonOutputFormatter with CompatibleJsonOutputFormatter to work around .NET 9 PipeWriter issue
+            var systemTextJsonFormatter = options.OutputFormatters
+                .FirstOrDefault(f => f.GetType().Name == "SystemTextJsonOutputFormatter");
+
+            if (systemTextJsonFormatter != null)
+            {
+                var index = options.OutputFormatters.IndexOf(systemTextJsonFormatter);
+                options.OutputFormatters[index] = new CompatibleJsonOutputFormatter(new JsonSerializerOptions());
+            }
         }
 
         public static IMvcBuilder WithApiControllers(this IMvcBuilder builder)
